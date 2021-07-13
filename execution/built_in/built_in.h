0cb0cc52ff03b5acc153a046dceac08ced3cd145
ergunirem <iremthedeveloper@gmail.com>
2021-07-13 22:50:41 +0200

    pwd working, cd & env in progress

---
+++ execution/built_in/built_in.h
@@ -0,0 +1,12 @@
+#ifndef BUILT_IN_H
+# define BUILT_IN_H
+
+# include "../minishell.h"
+# include <stdbool.h>
+
+bool	is_built_in(char *cmd);
+bool	exec_built_in(char **args, int argc);
+bool	exec_pwd(void);
+bool	exec_cd(char **arguments);
+
+#endif
