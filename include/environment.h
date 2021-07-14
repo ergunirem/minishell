09a300dd3833bf567cb2694c2e38108190990d3f
ergunirem <iremthedeveloper@gmail.com>
2021-07-14 22:05:55 +0200

    all includes in one directory

---
+++ include/environment.h
@@ -0,0 +1,49 @@
+#ifndef ENVIRONMENT_H
+# define ENVIRONMENT_H
+
+# include "minishell.h"
+
+/* Scope of Variables in Shell
+**
+** Environment: The outermost scope is the environment.
+** This is the only scope managed by the operating system.
+** It is therefore guaranteed to exist for every process.
+** When a process is started it receives a copy of its parent's environment.
+** Modifying the child's environment does not change the parent's,
+** modifying the parent's environment does not change that of an existing child.
+**
+** Shell Vars: When you assign a value to a variable in a shell,
+** if the variable is not in the environment yet it becomes a shell variable.
+** If it exists in the environment, environment var receives the new value.
+** New value is only for the shell itself and its subsequently forked children.
+** Shell variables only exist within the shell process.
+** They are never inherited by child processes.
+** There is a hierarchy of variable scopes, as expected.
+** Export copies a shell variable into the shell process's environment,
+** making it possible for child processes to inherit.
+*/
+
+// typedef struct s_pair
+// {
+// 	char	*key;
+// 	char	*value;
+// }	t_pair;
+
+typedef struct s_pair_lst
+{
+	char	*key;
+	char	*value;
+	struct s_pair_lst	*next;
+}	t_pair_lst;
+
+typedef struct s_env
+{
+	t_pair_lst	*env_vars;
+	t_pair_lst	*shell_vars;
+}	t_env;
+
+extern t_env	g_env;
+
+int	init_env_vars(char **envp);
+
+#endif
---
+++ include/minishell.h
@@ -0,0 +1,32 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../utils/libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include "token.h"
+# include "parser.h"
+# include "error.h"
+# include "built_in.h"
+# include "environment.h"
+# include <stdbool.h>
+
+// typedef enum { FALSE, TRUE } bool;
+
+void	lst_print(t_token *list);
+bool	check_list(t_token *tokens);
+bool	error_msg(char *msg);
+void	free_tokens(t_token **tokens);
+void	free_tree(t_tree_node *tree);
+void	free_array(char **array);
+int		execution(t_token *token, int nb, char **envp);
+void	execute_tree(t_tree_node *tree, char **envp);
+int		execute_program(char **argument, char **envp);
+void	ft_listadd_back(t_token **lst, t_token *new); //?move to libft
+
+#endif
