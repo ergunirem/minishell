cb9301813dd12093da6df8336e210bfbfa5d2b9f
ergunirem <iremthedeveloper@gmail.com>
2021-07-22 21:11:01 +0200

    var name check added & multiple vars unset

--- source/built_in/unset.c
+++ source/built_in/unset.c
@@ -3,12 +3,21 @@
 
 int	exec_unset(char **args, int argc)
 {
+	int	i;
+
 	if(argc == 1)
 		return (0);
-	if(ft_strchr(args[1], '='))
-		return (error_new_int(args[0], args[1], UNSET_ERR_MSG));
-	if(find_env_var(g_env.env_vars, args[1]))
-		remove_var(&g_env.env_vars, args[1]);
+	i = 1;
+	while(args[i])
+	{
+		if (!ft_isname(args[i]))
+			error_new_int(args[0], args[i], UNSET_ERR_MSG);
+		else if(ft_strchr(args[i], '='))
+			error_new_int(args[0], args[i], UNSET_ERR_MSG);
+		else if(find_env_var(g_env.env_vars, args[i]))
+			remove_var(&g_env.env_vars, args[i]);
+		i++;
+	}
 	// if(is_in(&g_env.shell_vars, args[1]))
 	// if(is_in_declare?(&g_env.env_vars, args[1]))
 	//unset from shell variables, just key declaration?
