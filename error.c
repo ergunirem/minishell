d8c8f6f10b93fdd59b9c259a36d88bbbc8312290
ergunirem <iremthedeveloper@gmail.com>
2021-07-25 21:57:41 +0200

    merge conflict deletes

--- error.c
+++
@@ -1,7 +0,0 @@
-#include "minishell.h"
-
-bool	error_msg(char *msg)
-{
-	ft_putstr_fd(msg, 0);
-	return (FALSE);
-}
--- error.h
+++
@@ -1,7 +0,0 @@
-#ifndef ERROR_H
-# define ERROR_H
-
-# define PIPE_ERROR_MSG "Minishell: syntax error near unexpected token `|'\n"
-# define REDIR_ERROR_MSG "Minishell: syntax error near unexpected token redirection\n"
-
-#endif
