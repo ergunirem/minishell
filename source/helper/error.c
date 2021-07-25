3c3af271b35b7c8fd0cb45c6072cdb72b13b5126
ergunirem <iremthedeveloper@gmail.com>
2021-07-25 22:28:34 +0200

    error funcs written to stderr

--- source/helper/error.c
+++ source/helper/error.c
@@ -7,26 +7,26 @@ bool	error_msg(char *msg)
 	return (false);
 }
 
-bool	error_new_bool(char *command, char *arg, char *msg)
+bool	error_new_bool(char *command, char *arg, char *msg, int fd)
 {
-	ft_putstr_fd("Minishell: ", 0);
-	ft_putstr_fd(command, 0);
-	ft_putstr_fd(": ", 0);
-	ft_putstr_fd(arg, 0);
-	ft_putstr_fd(": ", 0);
-	ft_putstr_fd(msg, 0);
-	ft_putstr_fd("\n", 0);
+	ft_putstr_fd("Minishell: ", fd);
+	ft_putstr_fd(command, fd);
+	ft_putstr_fd(": ", fd);
+	ft_putstr_fd(arg, fd);
+	ft_putstr_fd(": ", fd);
+	ft_putstr_fd(msg, fd);
+	ft_putstr_fd("\n", fd);
 	return (false);
 }
 
-int		error_new_int(char *command, char *arg, char *msg)
+int		error_new_int(char *command, char *arg, char *msg, int fd)
 {
-	ft_putstr_fd("Minishell: ", 0);
-	ft_putstr_fd(command, 0);
-	ft_putstr_fd(": ", 0);
-	ft_putstr_fd(arg, 0);
-	ft_putstr_fd(": ", 0);
-	ft_putstr_fd(msg, 0);
-	ft_putstr_fd("\n", 0);
+	ft_putstr_fd("Minishell: ", fd);
+	ft_putstr_fd(command, fd);
+	ft_putstr_fd(": ", fd);
+	ft_putstr_fd(arg, fd);
+	ft_putstr_fd(": ", fd);
+	ft_putstr_fd(msg, fd);
+	ft_putstr_fd("\n", fd);
 	return (1);
 }
