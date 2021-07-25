47db004af5701bfcffab6a54a154a05c2947bf95
ergunirem <iremthedeveloper@gmail.com>
2021-07-25 14:58:56 +0200

    echo without variable display

---
+++ source/built_in/echo.c
@@ -0,0 +1,44 @@
+#include "../../include/built_in.h"
+
+size_t	check_n_option(char *str)
+{
+	while (*str)
+	{
+		if (*str != 'n')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/*
+echo utility writes any specified operands, separated by single blank (` ')
+and followed by a newline (`\n') character, to the standard output (fd = 1)
+*/
+
+int	exec_echo(char **args, int argc)
+{
+	size_t	i;
+	size_t	n_option;
+
+	i = 1;
+	n_option = 0;
+	//variable display?
+	while (args[i] && args[i][0] == '-')
+	{
+		if (check_n_option(args[i] + 1) == 0)
+			break ;
+		n_option++;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (i != argc - 1)
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (n_option == 0)
+		ft_putstr_fd("\n", 1);
+	return (0);
+}
