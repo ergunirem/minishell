2f15c19297ef4a8b00ae6a9039b687d54c75cae7
ergunirem <iremthedeveloper@gmail.com>
2021-07-26 17:24:11 +0200

    ctx->fd added & comment added on what function does

--- source/built_in/unset.c
+++ source/built_in/unset.c
@@ -1,25 +1,30 @@
 #include "../../include/built_in.h"
-#include "../../include/environment.h"
 
-int	exec_unset(char **args, int argc)
+/*
+**	Ignores shell vars and variable names that does not exit in env vars
+**	Thus, unset only existing env vars
+**	Errs on invalid identifier '-': unset -u
+**	Errs on variable names with assignment '=': unset x=y
+**	Errs on vars if invalid variable name is given: unset var@iable
+**	On multiple arguments it ignores errors and unsets valid variable names
+*/
+
+int	exec_unset(char **args, int argc, t_context *ctx)
 {
 	int	i;
 
-	if(argc == 1)
+	if (argc == 1)
 		return (0);
 	i = 1;
-	while(args[i])
+	while (args[i])
 	{
 		if (!ft_isname(args[i]))
-			error_new_int(args[0], args[i], UNSET_ERR_MSG, 1);
-		else if(ft_strchr(args[i], '='))
-			error_new_int(args[0], args[i], UNSET_ERR_MSG, 1);
-		else if(find_env_var(g_env.env_vars, args[i]))
+			error_new_int(args[0], args[i], IDENTIFIER_ERR_MSG, ctx->fd[2]);
+		else if (ft_strchr(args[i], '='))
+			error_new_int(args[0], args[i], IDENTIFIER_ERR_MSG, ctx->fd[2]);
+		else if (find_env_var(g_env.env_vars, args[i]))
 			remove_var(&g_env.env_vars, args[i]);
 		i++;
 	}
-	// if(is_in(&g_env.shell_vars, args[1]))
-	// if(is_in_declare?(&g_env.env_vars, args[1]))
-	//unset from shell variables, just key declaration?
 	return (0);
 }
