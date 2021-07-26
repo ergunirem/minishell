bbff500da24cac1803a4cd11f02264df68b741d5
ergunirem <iremthedeveloper@gmail.com>
2021-07-26 17:26:32 +0200

    ctx->fd added

--- source/built_in/env.c
+++ source/built_in/env.c
@@ -1,23 +1,22 @@
 #include "../../include/built_in.h"
-#include "../../include/environment.h"
 
 /*
 **	Returns error if env is called with other arguments.
 */
 
-int	exec_env(char **args, int argc)
+int	exec_env(char **args, int argc, t_context *ctx)
 {
-	t_pair_lst *env_lst;
+	t_pair_lst	*env_lst;
 
-	if(argc != 1)
-		return (error_msg(ENV_ERR_MSG));
+	if (argc != 1)
+		return (error_new_int("env", "", ENV_ERR_MSG, ctx->fd[2]));
 	env_lst = g_env.env_vars;
-	while(env_lst)
+	while (env_lst)
 	{
-		ft_putstr_fd(env_lst->key, 0);
-		ft_putstr_fd("=", 0);
-		ft_putstr_fd(env_lst->value, 0);
-		ft_putstr_fd("\n", 0);
+		ft_putstr_fd(env_lst->key, ctx->fd[1]);
+		ft_putstr_fd("=", ctx->fd[1]);
+		ft_putstr_fd(env_lst->value, ctx->fd[1]);
+		ft_putstr_fd("\n", ctx->fd[1]);
 		env_lst = env_lst->next;
 	}
 	return (0);
