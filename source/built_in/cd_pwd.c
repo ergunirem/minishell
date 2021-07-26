6bffe4f48f60a96800fb8064152deaaf2e432711
ergunirem <iremthedeveloper@gmail.com>
2021-07-26 20:00:07 +0200

    cd - added

--- source/built_in/cd_pwd.c
+++ source/built_in/cd_pwd.c
@@ -1,51 +1,58 @@
 #include "../../include/built_in.h"
-#include "../../include/minishell.h"
 
 int	exec_pwd(t_context *ctx)
 {
 	char	*path;
 
 	path = getcwd(NULL, 0);
+	if (!path)
+		return (error_new_int("pwd", "getcwd", strerror(errno), ctx->fd[2]));
 	ft_putstr_fd(path, ctx->fd[1]);
 	ft_putstr_fd("\n", ctx->fd[1]);
 	free(path);
 	return (0);
 }
 
-bool	change_dir_var(t_pair_lst *lst, char *var_name)
+int	change_dir_var(t_pair_lst *lst, char *var_name, int fd_err)
 {
 	t_pair_lst	*env_var;
 	char		*path;
 
 	path = getcwd(NULL, 0);
-	// if (!path)
-		// return (error_new_bool("cd", "getcwd", strerror(errno)));
+	if (!path)
+		return (error_new_int("cd", "getcwd", strerror(errno), fd_err));
 	env_var = find_env_var(lst, var_name);
 	free(env_var->value);
 	env_var->value = ft_strdup(path);
 	if (!env_var->value)
-		return (error_new_bool("cd", "ft_stdrup", "Malloc failed\n", 1));
+		return (error_new_int("cd", "ft_stdrup", "Malloc failed\n", fd_err));
 	free(path);
 	return (true);
 }
 
-bool	exec_cd(char **arguments)
+int	exec_cd(char **arguments, t_context *ctx)
 {
 	t_pair_lst	*env_var;
 	char		*tmp;
 
-	change_dir_var(g_env.env_vars, "OLDPWD");
+	if (ft_strncmp(arguments[1], "-", 2) == 0)
+	{
+		env_var = find_env_var(g_env.env_vars, "OLDPWD");
+		//prints out the oldpwd when cd -
+		tmp = ft_strdup(env_var->value);
+	}
+	change_dir_var(g_env.env_vars, "OLDPWD", ctx->fd[2]);
 	if (!arguments[1])
 	{
 		env_var = find_env_var(g_env.env_vars, "HOME");
 		if (chdir(env_var->value) == -1)
-			return (error_new_bool("cd", env_var->value, strerror(errno), 1));
-		return(change_dir_var(g_env.env_vars, "PWD"));
+			return (error_new_int("cd", env_var->value, strerror(errno), ctx->fd[2]));
+		return(change_dir_var(g_env.env_vars, "PWD", ctx->fd[2]));
 	}
-	//should we get rid of quotes in lexer part?
-	tmp = ft_strtrim(arguments[1], "\""); //any issue with using both " and '
+	if (ft_strncmp(arguments[1], "-", 2) != 0)
+		tmp = ft_strtrim(arguments[1], "\""); //any issue with using both " and '
 	if (chdir(tmp) == -1)
-		return (error_new_bool("cd", tmp, strerror(errno), 1));
+		return (error_new_int("cd", tmp, strerror(errno), ctx->fd[2]));
 	free(tmp);
-	return (change_dir_var(g_env.env_vars, "PWD"));
+	return (change_dir_var(g_env.env_vars, "PWD", ctx->fd[2]));
 }
