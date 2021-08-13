f859536a213fad6b05e8f21e3156eb5d780ef4da
xxj <xxj>
2021-08-13 14:12:29 +0200

    update

--- source/exec/exec_complex_cmd.c
+++ source/exec/exec_complex_cmd.c
@@ -14,9 +14,8 @@ int	count_whitespace(char *str)
 			count++;
 		i++;
 	}
-	// printf("count is %d\n", count);
 	if (ft_iswhitespace(str[0]) && i == 0)
-		return (-1);//env is only white space =>should only pomp a new line
+		return (-1);
 	else if (ft_iswhitespace(str[0]))
 		return (count - 1);
 	else
@@ -31,17 +30,18 @@ void	parse_nonwhitesp(char *str, char **argv, int *k)
 	i = 0;
 	while (str[i])
 	{
-		if ((i == 0 && (!ft_iswhitespace(str[i]))) || (!ft_iswhitespace(str[i]) && i > 0 && ft_iswhitespace(str[i - 1])))
+		if ((i == 0 && (!ft_iswhitespace(str[i]))) || (!ft_iswhitespace(str[i])
+				&& i > 0 && ft_iswhitespace(str[i - 1])))
 		{
 			len = 0;
 			while (str[i + len])
 			{
-				if (!ft_iswhitespace(str[i + len]) && (!str[i + len + 1] || ft_iswhitespace(str[i + len + 1])))
+				if (!ft_iswhitespace(str[i + len]) && (!str[i + len + 1]
+						|| ft_iswhitespace(str[i + len + 1])))
 				{
 					argv[*k] = ft_substr(str, i, len + 1);
-					// printf("argv %d is %s i is %d len is %d\n", *k, argv[*k], i, len);
 					(*k)++;
-					break;
+					break ;
 				}
 				len++;
 			}
@@ -53,10 +53,10 @@ void	parse_nonwhitesp(char *str, char **argv, int *k)
 
 int	check_complexcmd (char ***argv, int *argc)
 {
-	int	i;
-	int	j;
-	int	k;
-	char **argv2;
+	int		i;
+	int		j;
+	int		k;
+	char	**argv2;
 
 	i = count_whitespace((*argv)[0]);
 	if (i == -1)
@@ -69,11 +69,9 @@ int	check_complexcmd (char ***argv, int *argc)
 		return (error_new_bool(NULL, NULL, strerror(errno), 2));
 	argv2[argc[0] + i] = NULL;
 	parse_nonwhitesp((*argv)[0], argv2, &j);
-	// printf("j is %d and i is %d\n", j, i);
 	while (j < argc[0] + 1 + i)
 	{
 		argv2[j] = ft_strdup((*argv)[j - i]);
-		// printf("argv2 %d is %s\n", j, argv2[j]);
 		j++;
 	}
 	free_array(*argv);
